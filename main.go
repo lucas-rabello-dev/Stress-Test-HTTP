edb2c42b208ed973de656e6005c26c6244d67b3d
Lucas Rabello <lucasrabello2020@gmail.com>
2025-09-17 18:08:41 -0300

    Update

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+// a main vai pegar as flags com cobra
+// concorrência aqui 
+
+import (
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	
+}
---
+++ myapp/main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+
+*/
+package main
+
+import "github.com/lucas-rabello-dev/Stress-Test-HTTP/cmd"
+
+func main() {
+	cmd.Execute()
+}
